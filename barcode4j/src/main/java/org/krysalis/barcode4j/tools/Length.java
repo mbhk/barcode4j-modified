333d78a446f488c70f2dbca8a15f305f55414708
Michael Krueger <mbh.krueger@gmail.com>
2015-02-02 00:08:49 +0100

    New Length.Builder API

--- barcode4j/src/main/java/org/krysalis/barcode4j/tools/Length.java
+++ barcode4j/src/main/java/org/krysalis/barcode4j/tools/Length.java
@@ -15,6 +15,11 @@
  */
 package org.krysalis.barcode4j.tools;
 
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * This class represents a length (value plus unit). It is used to parse 
  * expressions like "0.21mm".
@@ -67,13 +72,15 @@ public class Length {
      * Parses a value with unit.
      * @param text the String to parse
      * @param defaultUnit the default unit to assume
+     * @deprecated use new {@link Length.Builder} instead
      */
-    protected void parse(String text, String defaultUnit) {
+    @Deprecated
+    protected final void parse(String text, String defaultUnit) {
         final String s = text.trim();
         if (s.length() == 0) {
             throw new IllegalArgumentException("Length is empty");
         }
-        StringBuffer sb = new StringBuffer(s.length());
+        StringBuilder sb = new StringBuilder(s.length());
         int mode = 0;
         int i = 0;
         while (i < s.length()) {
@@ -161,9 +168,127 @@ public class Length {
         }
     }
     
-    /** {@inheritDoc} */
+    /**
+     * Gets als supported unit types.
+     *
+     * @return List of supported unit
+     */
+    public static Set<String> getSupportedUnits() {
+        Set<String> res = new TreeSet<String>();
+        res.add(CM);
+        res.add(INCH);
+        res.add(POINT);
+        res.add(MM);
+        return res;
+    }
+
+    @Override
+    public int hashCode() {
+        return toString().hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Length other = (Length) obj;
+        if (Double.doubleToLongBits(this.value) != Double.doubleToLongBits(other.value)) {
+            return false;
+        }
+        if ((this.unit == null) ? (other.unit != null) : !this.unit.equals(other.unit)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
     public String toString() {
         return getValue() + getUnit();
     }
     
+    public static class Builder {
+        private Double value = null;
+        private String unit = null;
+        private final Set<String> supportedUnits;
+        private static final Pattern UNIT_PATTERN = Pattern.compile("[\\s]*([a-zA-Z]+)$");
+        private static final Pattern VALUE_PATTERN = Pattern.compile("^(([\\d]+)|([\\d]?\\.[\\d]+))[\\s]*");
+
+        public Builder() {
+            supportedUnits = Length.getSupportedUnits();
+        }
+
+        public Builder withValue(double value) {
+            this.value = value;
+            return this;
+        }
+
+        public Builder withValue(String value) {
+            this.value = parseValue(value);
+            return this;
+        }
+
+        public Builder withUnit(String unit) {
+            this.unit = checkUnit(unit);
+            return this;
+        }
+
+        public Builder fromString(String valueWithUnit) {
+            if (valueWithUnit == null) {
+                throw new IllegalArgumentException("argument must not be null");
+            }
+            String tmp = valueWithUnit.trim();
+            this.unit = checkUnit(extractUnit(tmp));
+            this.value = parseValue(extractValue(tmp));
+            return this;
+        }
+
+        public Length build() {
+            if (value == null || unit == null) {
+                throw new IllegalStateException("you have to specify value and unit " + value + unit);
+            }
+            return new Length(value, unit);
+        }
+
+        public void reset() {
+            this.unit = null;
+            this.value = null;
+        }
+
+        private double parseValue(String value) {
+            return Double.parseDouble(value);
+        }
+
+        private String extractUnit(String valueWithUnit) {
+            Matcher m = UNIT_PATTERN.matcher(valueWithUnit);
+            if (m.find()) {
+                return m.group();
+            } else {
+                throw new IllegalArgumentException("unit not found in " + valueWithUnit);
+            }
+        }
+
+        private String extractValue(String valueWithUnit) {
+            Matcher m = VALUE_PATTERN.matcher(valueWithUnit);
+            if (m.find()) {
+                return m.group();
+            } else {
+                throw new IllegalArgumentException("value not found in " + valueWithUnit);
+            }
+        }
+
+        private String checkUnit(String unit) {
+            if(unit == null) {
+                throw new IllegalArgumentException("unit must not be null");
+            }
+            String tmp = unit.trim().toLowerCase();
+            if (!supportedUnits.contains(tmp)) {
+                throw new IllegalArgumentException("Unsupported unit " + tmp);
+            }
+            return tmp;
+        }
+    }
 }
